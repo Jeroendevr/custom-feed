da1208436853848b7d8cd5154bf515d4d22b1457
Jeroen de Vries <jeroendevries@runbox.eu>
2022-02-06 14:24:33 +0100

    basic working example with xml response

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-from flask import Flask
+from apiflask import APIFlask, Schema, input, output
+from apiflask.fields import Integer, String
+from apiflask.validators import Length, OneOf
 from json2xml import json2xml
 from json2xml.utils import readfromjson
 
-app = Flask((__name__))
 
-@app.route("/sample.xml")
-def hello():
+class PetInSchema(Schema):
+    name = String(required=True, validate=Length(0, 10))
+    category = String(required=True, validate=OneOf(['dog', 'cat']))
+
+class PetOutSchema(Schema):
+    id = Integer()
+    name = String()
+    category = String()
+
+
+app = APIFlask(__name__, title='Wonderful API', version='1.0')
+
+@app.get('/')
+def index():
+    return {'message': 'hello'}
+
+@app.get('/pets/<int:pet_id>')
+# @output(PetOutSchema)
+def get_pet(pet_id):
     data = readfromjson("sampledata.json")
-    data = json2xml.Json2xml(data, wrapper="all", pretty=True).to_xml()
+    return json2xml.Json2xml(data).to_xml()
+
+
+@app.post('/pets')
+@input(PetInSchema)
+def create_pet(data):
     print(data)
-    return data
+    return {'message': 'created'}, 201
+
+
+@app.put('/pets/<int:pet_id>')
+def update_pet(pet_id):
+    return {'message': 'updated'}
+
+
+@app.delete('/pets/<int:pet_id>')
+def delete_pet(pet_id):
+    return '', 204
